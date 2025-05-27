317739bfeba1829766ce8983590ced102c6bcebd
farrelputras <fernandiofarrel@gmail.com>
2025-05-28 00:42:33 +0700

    fix: search bar component

--- src/app/(main)/penyewaan/[id]/page.tsx
+++ src/app/(main)/penyewaan/[id]/page.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft, Wallet, Clock, FileText, Info } from "lucide-react";
 import { useParams } from "next/navigation";
 import Image from "next/image";
 import motorList from "@/app/data/motorlist"; // pastikan path-nya sesuai struktur project kamu
+import SearchBar from "@/components/SearchBar";
 
 const RentalDetailPage = () => {
   const { id } = useParams();
@@ -14,8 +15,12 @@ const RentalDetailPage = () => {
   if (!motor) {
     return (
       <main className="p-6 max-w-3xl mx-auto text-center">
-        <h1 className="text-xl font-semibold text-red-600">Motor tidak ditemukan</h1>
-        <p className="text-gray-600 mt-2">Pastikan URL sudah benar atau pilih motor lain.</p>
+        <h1 className="text-xl font-semibold text-red-600">
+          Motor tidak ditemukan
+        </h1>
+        <p className="text-gray-600 mt-2">
+          Pastikan URL sudah benar atau pilih motor lain.
+        </p>
       </main>
     );
   }
@@ -32,25 +37,21 @@ const RentalDetailPage = () => {
   };
 
   return (
-    <main className="p-6 max-w-screen mx-3">
-        <div className="flex items-center gap-4 mb-4 bg-white rounded-xl shadow-sm border border-gray-200 p-4">
-            {/* Tombol back */}
-            <Link href="/penyewaan" className="text-blue-600 hover:text-blue-800">
-                <ArrowLeft className="w-6 h-6" />
-            </Link>
-
-            {/* Judul dan tanggal */}
-            <div>
-            <h1 className="text-xl font-semibold">Rental Motor</h1>
-            <p className="text-sm text-gray-600">{rentalPeriod.start} - {rentalPeriod.end}</p>
-            </div>
-        </div>
+    <main className="w-full mt-10 mx-15 p-4">
+      <>
+        <SearchBar />
+      </>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Motor Info */}
         <div className="md:col-span-2 bg-white shadow rounded-lg p-6">
           <div className="flex gap-4">
-            <Image src={motor.image} alt={motor.name} width={200} height={100} />
+            <Image
+              src={motor.image}
+              alt={motor.name}
+              width={200}
+              height={100}
+            />
             <div>
               <h2 className="text-2xl font-bold">{motor.name}</h2>
               <div className="flex gap-4 text-lg mt-2 text-black font-bold">
@@ -59,11 +60,16 @@ const RentalDetailPage = () => {
                 <span>🪑 {motor.seats}</span>
               </div>
               <div className="flex flex-wrap gap-2 mt-3">
-                {["Anti Theft Alarm", "12L Baggages", "LED Headlights"].map((feature, idx) => (
-                  <span key={idx} className="text-xs bg-blue-600 text-white font-bold py-1.5 px-4 rounded-full">
-                    {feature}
-                  </span>
-                ))}
+                {["Anti Theft Alarm", "12L Baggages", "LED Headlights"].map(
+                  (feature, idx) => (
+                    <span
+                      key={idx}
+                      className="text-xs bg-blue-600 text-white font-bold py-1.5 px-4 rounded-full"
+                    >
+                      {feature}
+                    </span>
+                  )
+                )}
               </div>
             </div>
           </div>
@@ -71,12 +77,15 @@ const RentalDetailPage = () => {
           {/* Rental Policy */}
           <div className="mt-6 p-4 bg-white rounded-xl shadow-sm border border-gray-200">
             <h3 className="font-semibold mb-2 flex items-center gap-2">
-                <FileText className="w-6 h-6 text-blue-600" />
-                Rental Policy
+              <FileText className="w-6 h-6 text-blue-600" />
+              Rental Policy
             </h3>
             <ul className="text-sm list-disc list-inside text-gray-700">
               <li>Batas waktu penggunaan maksimal 24 jam per hari sewa</li>
-              <li>Penggunaan dibatasi hanya dalam kota. Jika digunakan di luar kota, biaya tambahan akan dikenakan</li>
+              <li>
+                Penggunaan dibatasi hanya dalam kota. Jika digunakan di luar
+                kota, biaya tambahan akan dikenakan
+              </li>
               <li>Kembalikan bahan bakar sesuai dengan saat diterima</li>
             </ul>
           </div>
@@ -84,18 +93,21 @@ const RentalDetailPage = () => {
           {/* Important Info */}
           <div className="mt-4 p-4 bg-white rounded-xl shadow-sm border border-gray-200">
             <h3 className="font-semibold mb-2 flex items-center gap-2">
-                <Info className="w-6 h-6 text-yellow-500" />
-                Important Information
+              <Info className="w-6 h-6 text-yellow-500" />
+              Important Information
             </h3>
             <ul className="text-sm mb-1 list-disc list-inside text-black">
-                <strong>Sebelum memesan:</strong>
-                <li className="text-gray-700">
-                    Pastikan Anda sudah membaca semua kebutuhan untuk menyewa.
-                </li>
-                <strong>Setelah memesan:</strong>
-                <li className="text-gray-700">
-                    Pihak <span className="text-blue-500 font-semibold">RentalQ</span> akan mengontak penyewa melalui nomor yang dicantumkan untuk verifikasi.
-                </li>
+              <strong>Sebelum memesan:</strong>
+              <li className="text-gray-700">
+                Pastikan Anda sudah membaca semua kebutuhan untuk menyewa.
+              </li>
+              <strong>Setelah memesan:</strong>
+              <li className="text-gray-700">
+                Pihak{" "}
+                <span className="text-blue-500 font-semibold">RentalQ</span>{" "}
+                akan mengontak penyewa melalui nomor yang dicantumkan untuk
+                verifikasi.
+              </li>
             </ul>
           </div>
         </div>
@@ -107,9 +119,22 @@ const RentalDetailPage = () => {
             Rental Duration
           </h3>
           <div className="text-lg text-gray-700">
-            <p>Start: <span className="text-blue-600 font-medium">{rentalPeriod.start}</span></p>
-            <p>End: <span className="text-blue-600 font-medium">{rentalPeriod.end}</span></p>
-            <p className="mt-1">Duration: <span className="font-semibold">{rentalPeriod.days} Day(s)</span></p>
+            <p>
+              Start:{" "}
+              <span className="text-blue-600 font-medium">
+                {rentalPeriod.start}
+              </span>
+            </p>
+            <p>
+              End:{" "}
+              <span className="text-blue-600 font-medium">
+                {rentalPeriod.end}
+              </span>
+            </p>
+            <p className="mt-1">
+              Duration:{" "}
+              <span className="font-semibold">{rentalPeriod.days} Day(s)</span>
+            </p>
           </div>
 
           <hr className="my-4" />
@@ -120,19 +145,21 @@ const RentalDetailPage = () => {
           </h3>
           <div className="text-md text-gray-700 space-y-1">
             <p className="flex justify-between">
-                <span>Basic Rental:</span>
-                <span>Rp{price.basic.toLocaleString()}</span>
+              <span>Basic Rental:</span>
+              <span>Rp{price.basic.toLocaleString()}</span>
             </p>
             <p className="flex justify-between">
-                <span>Pick-up in other location:</span>
-                <span>Rp0</span>
+              <span>Pick-up in other location:</span>
+              <span>Rp0</span>
             </p>
             <p className="flex justify-between">
-                <span>Taxes & Fees:</span>
-                <span>Rp0</span>
+              <span>Taxes & Fees:</span>
+              <span>Rp0</span>
             </p>
             <p className="font-semibold">Total Price: </p>
-            <p className="text-lg font-bold mt-2 text-blue-600">Rp{price.total.toLocaleString()}</p>
+            <p className="text-lg font-bold mt-2 text-blue-600">
+              Rp{price.total.toLocaleString()}
+            </p>
           </div>
 
           <button className="mt-4 w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,38 @@
-'use client';
+"use client";
 
+import Link from "next/link";
 import { Button } from "@/components/Button";
-import { Search } from "lucide-react";
+import { ArrowLeft, Search } from "lucide-react";
+import motorList from "@/app/data/motorlist";
 
 export default function SearchBar() {
-    return (
-      <div className="bg-white shadow-md p-4 rounded-xl mb-4 flex items-center justify-between">
+  const rentalPeriod = {
+    start: "Thursday, 17 April 2025 10:00 WIB",
+    end: "Sunday, 20 April 2025 10:00 WIB",
+    days: 2,
+  };
+
+  return (
+    <div className="flex items-center justify-between mb-4 bg-white rounded-xl shadow-sm border border-gray-200 p-4">
+      <div className="flex items-center gap-3">
+        {/* Tombol back */}
+        <Link href="/penyewaan" className="text-blue-600 hover:text-blue-800">
+          <ArrowLeft className="w-6 h-6" />
+        </Link>
+
+        {/* Judul dan tanggal */}
         <div>
-          <h2 className="text-lg font-semibold">Rental Motor</h2>
-          <p className="text-sm text-gray-500">Thu, 17 April 2025, 10:00 WIB - Sun, 20 April 2025, 10:00 WIB</p>
+          <h1 className="text-xl font-semibold">Rental Motor</h1>
+          <p className="text-sm text-gray-500">
+            {rentalPeriod.start} - {rentalPeriod.end}
+          </p>
         </div>
-        <Button variant="outline" className="flex gap-2 items-center">
-          <Search size={16} /> Change Search
-        </Button>
       </div>
-    );
+
+      {/* Change Search */}
+      <Button variant="outline" className="flex gap-2 items-center">
+        <Search size={16} /> Change Search
+      </Button>
+    </div>
+  );
 }
